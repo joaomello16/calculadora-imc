87286f49cdcb33c2e12d057327998c12445b2f1f
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-06-23 16:31:51 -0300

    mostra os resultados no console

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-let peso = 0;
-let altura =0;
+function add(){
+    const masc = document.getElementById('masc')
+    const fem = document.getElementById('fem')
+
+    const question1 = document.getElementById('peso')
+    const question2 = document.getElementById('altura')
+
+    
+    const peso = question1.value
+    const altura = question2.value
+
+    
+    console.log(peso)
+    console.log(altura)
+
+    const imc = peso/(altura*altura)
+    console.log(imc)
+
+    if(masc.checked){
+        console.log('masculino')
+    }else if(fem.checked){
+        console.log('feminino')
+    }
+    question1.value = ''
+    question2.value = ''
+}
